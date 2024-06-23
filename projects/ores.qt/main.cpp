18bbd9faa7571495ef21e3da3485f083713b4b74
Marco Craveiro <marco.craveiro@gmail.com>
2024-06-23 16:53:15 +0100

    qt: add basic support for splash screen

--- projects/ores.qt/main.cpp
+++ projects/ores.qt/main.cpp
@@ -1,12 +1,22 @@
-#include "ui_main_window.h"
+#include <QTimer>
 #include <QApplication>
+#include <QSplashScreen>
+#include "ui_main_window.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+
+    QSplashScreen splash;
+    splash.setPixmap(QPixmap("splash_screen.png"));
+    splash.show();
+
     QMainWindow mainWindow;
     Ui::MainWindow window;
     window.setupUi(&mainWindow);
-    mainWindow.show();
+
+    QTimer::singleShot(1000, &splash, SLOT(close()));
+    QTimer::singleShot(1000, &mainWindow, SLOT(show()));
+
     return QApplication::exec();
 }

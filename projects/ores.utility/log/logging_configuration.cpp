bb0fd249ef22a088ccb62df92a1c88ab96d39155
Marco Craveiro <marco.craveiro@gmail.com>
2024-06-28 00:44:26 +0100

    utility: add basic logging support

---
+++ projects/ores.utility/log/logging_configuration.cpp
@@ -0,0 +1,93 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * Copyright (C) 2024 Marco Craveiro <marco.craveiro@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+#include "ores.utility/log/logging_configuration.hpp"
+
+namespace ores::utility::log {
+
+logging_configuration::logging_configuration()
+    : output_to_console_(false) { }
+
+logging_configuration::logging_configuration(logging_configuration&& rhs) noexcept
+    : severity_(std::move(rhs.severity_)),
+      filename_(std::move(rhs.filename_)),
+      output_to_console_(rhs.output_to_console_),
+      output_directory_(std::move(rhs.output_directory_)) { }
+
+logging_configuration::logging_configuration(
+    std::string severity, std::string filename,
+    bool output_to_console, boost::filesystem::path output_directory)
+    : severity_(std::move(severity)), filename_(std::move(filename)),
+      output_to_console_(output_to_console),
+      output_directory_(std::move(output_directory)) { }
+
+void logging_configuration::swap(logging_configuration& other) noexcept {
+    using std::swap;
+    swap(severity_, other.severity_);
+    swap(filename_, other.filename_);
+    swap(output_to_console_, other.output_to_console_);
+    swap(output_directory_, other.output_directory_);
+}
+
+bool logging_configuration::operator==(const logging_configuration& rhs) const {
+    return severity_ == rhs.severity_ &&
+        filename_ == rhs.filename_ &&
+        output_to_console_ == rhs.output_to_console_ &&
+        output_directory_ == rhs.output_directory_;
+}
+
+logging_configuration& logging_configuration::operator=(logging_configuration other) {
+    using std::swap;
+    swap(*this, other);
+    return *this;
+}
+
+std::string logging_configuration::severity() const {
+    return severity_;
+}
+
+void logging_configuration::severity(std::string v) {
+    severity_ = std::move(v);
+}
+
+std::string logging_configuration::filename() const {
+    return filename_;
+}
+
+void logging_configuration::filename(std::string v) {
+    filename_ = std::move(v);
+}
+
+bool logging_configuration::output_to_console() const {
+    return output_to_console_;
+}
+
+void logging_configuration::output_to_console(bool v) {
+    output_to_console_ = v;
+}
+
+boost::filesystem::path logging_configuration::output_directory() const {
+    return output_directory_;
+}
+
+void logging_configuration::output_directory(boost::filesystem::path v) {
+    output_directory_ = std::move(v);
+}
+
+}
---
+++ projects/ores.utility/log/logging_configuration.hpp
@@ -0,0 +1,108 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * Copyright (C) 2024 Marco Craveiro <marco.craveiro@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+#ifndef ORES_UTILITY_LOG_LOGGING_CONFIGURATION_HPP
+#define ORES_UTILITY_LOG_LOGGING_CONFIGURATION_HPP
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+#pragma once
+#endif
+
+#include <string>
+#include <algorithm>
+#include <boost/filesystem/path.hpp>
+
+namespace ores::utility::log {
+
+/**
+ * @brief Options related to logging.
+ */
+class logging_configuration final {
+public:
+    logging_configuration(const logging_configuration&) = default;
+    ~logging_configuration() = default;
+    logging_configuration();
+    logging_configuration(logging_configuration&& rhs) noexcept;
+
+    logging_configuration(std::string severity, std::string filename,
+        bool output_to_console, boost::filesystem::path output_directory);
+
+    /**
+     * @brief Level at which to log.
+     */
+    /**@{*/
+    std::string severity() const;
+    void severity(std::string v);
+    /**@}*/
+
+    /**
+     * @brief Name of the file to log into.
+     *
+     * If empty, file logging is disabled.
+     */
+    /**@{*/
+    std::string filename() const;
+    void filename(std::string v);
+    /**@}*/
+
+    /**
+     * @brief If true, dumps the log into the console.
+     */
+    /**@{*/
+    bool output_to_console() const;
+    void output_to_console(bool v);
+    /**@}*/
+
+    /**
+     * @brief Directory in which to place the output.
+     */
+    /**@{*/
+    boost::filesystem::path output_directory() const;
+    void output_directory(boost::filesystem::path v);
+    /**@}*/
+
+    bool operator==(const logging_configuration& rhs) const;
+    bool operator!=(const logging_configuration& rhs) const {
+        return !this->operator==(rhs);
+    }
+
+    void swap(logging_configuration& other) noexcept;
+    logging_configuration& operator=(logging_configuration other);
+
+private:
+    std::string severity_;
+    std::string filename_;
+    bool output_to_console_;
+    boost::filesystem::path output_directory_;
+};
+
+}
+
+namespace std {
+
+template<>
+inline void swap(
+    ores::utility::log::logging_configuration& lhs,
+    ores::utility::log::logging_configuration& rhs) {
+    lhs.swap(rhs);
+}
+
+}
+
+#endif

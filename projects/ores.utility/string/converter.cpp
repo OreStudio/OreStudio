4d654d874f73fe6903ee9a8734a3743082b5c1a7
Marco Craveiro <marco.craveiro@gmail.com>
2024-07-11 00:23:30 +0100

    utility: add string conversion

---
+++ projects/ores.utility/string/converter.cpp
@@ -0,0 +1,58 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * Copyright (C) 2024 Marco Craveiro <marco.craveiro@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+#include <charconv>
+#include "ores.utility/log/logger.hpp"
+#include "ores.utility/string/conversion_error.hpp"
+#include "ores.utility/string/converter.hpp"
+
+namespace {
+
+using namespace ores::utility::log;
+auto lg(logger_factory("ores.utility.string.converter"));
+
+const std::string invalid_argument("Invalid argument: ");
+const std::string out_of_range("Out of range: ");
+const std::string other_conversion_error("Unspecified conversion error: ");
+
+}
+
+namespace ores::utility::string {
+
+int convert_to_int(std::string s, int base) {
+    BOOST_LOG_SEV(lg, debug) << "Converting to int: " << s;
+    int r;
+    auto [ptr, ec] = std::from_chars(s.data(), s.data() + s.size(), r, base);
+
+     if (ec == std::errc()) {
+        BOOST_LOG_SEV(lg, debug) << "Converted successfully: " << r;
+        return r;
+    } else if (ec == std::errc::invalid_argument) {
+        BOOST_LOG_SEV(lg, error) << invalid_argument << s;
+        BOOST_THROW_EXCEPTION(conversion_error(invalid_argument + s));
+    } else if (ec == std::errc::result_out_of_range) {
+        BOOST_LOG_SEV(lg, error) << invalid_argument << s;
+        BOOST_THROW_EXCEPTION(conversion_error(out_of_range + s));
+     } else {
+         BOOST_LOG_SEV(lg, error) << other_conversion_error << s;
+         BOOST_THROW_EXCEPTION(conversion_error(other_conversion_error + s));
+     }
+}
+
+}
---
+++ projects/ores.utility/string/converter.hpp
@@ -0,0 +1,42 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * Copyright (C) 2024 Marco Craveiro <marco.craveiro@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+#ifndef ORES_UTILITY_STRING_CONVERTER_HPP
+#define ORES_UTILITY_STRING_CONVERTER_HPP
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+#pragma once
+#endif
+
+#include <string>
+#include <charconv>
+
+namespace ores::utility::string {
+
+/**
+ * @brief Converts a string into an integer.
+ *
+ * @param s string to convert.
+ * @param base numeric base to use.
+ */
+int convert_to_int(std::string s, int base = 10);
+
+}
+
+#endif

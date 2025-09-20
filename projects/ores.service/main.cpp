68cd5ce07abee84233bea9c54ebc4d8ec6941ab2
Marco Craveiro <marco.craveiro@gmail.com>
2025-09-20 17:54:32 +0100

    [server] Minor cosmetic fixes to server

--- projects/ores.service/main.cpp
+++ projects/ores.service/main.cpp
@@ -25,68 +25,50 @@
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/asio/signal_set.hpp>
 #include <boost/asio/write.hpp>
-#include <list>
+#include <array>
 
-// tag::decls[]
 namespace cobalt = boost::cobalt;
 using boost::asio::ip::tcp;
-using boost::asio::detached;
 using tcp_acceptor = cobalt::use_op_t::as_default_on_t<tcp::acceptor>;
 using tcp_socket   = cobalt::use_op_t::as_default_on_t<tcp::socket>;
-namespace this_coro = boost::cobalt::this_coro;
-//end::decls[]
 
-// tag::echo[]
-cobalt::promise<void> echo(tcp_socket socket)
-{
-  try // <1>
-  {
-    char data[4096];
-    while (socket.is_open()) // <2>
-    {
-      std::size_t n = co_await socket.async_read_some(boost::asio::buffer(data)); // <3>
-      co_await async_write(socket, boost::asio::buffer(data, n)); // <4>
+namespace {
+
+cobalt::promise<void> echo(tcp_socket socket) {
+    try {
+        std::array<char, 4096> data;
+        while (socket.is_open()) {
+            std::size_t n = co_await socket.async_read_some(boost::asio::buffer(data)); // <3>
+            co_await async_write(socket, boost::asio::buffer(data, n));
+        }
+    } catch (std::exception& e) {
+        std::printf("echo: exception: %s\n", e.what());
     }
-  }
-  catch (std::exception& e)
-  {
-    std::printf("echo: exception: %s\n", e.what());
-  }
 }
-// end::echo[]
 
-
-// tag::listen[]
-cobalt::generator<tcp_socket> listen()
-{
-  tcp_acceptor acceptor({co_await cobalt::this_coro::executor}, {tcp::v4(), 55555});
-  for (;;) // <1>
-  {
-    tcp_socket sock = co_await acceptor.async_accept(); // <2>
-    co_yield std::move(sock); // <3>
-  }
-  co_return tcp_socket{acceptor.get_executor()}; // <4>
+cobalt::generator<tcp_socket> listen() {
+    tcp_acceptor acceptor({co_await cobalt::this_coro::executor}, {tcp::v4(), 55555});
+    for (;;) {
+        tcp_socket sock = co_await acceptor.async_accept();
+        co_yield std::move(sock);
+    }
+    co_return tcp_socket{acceptor.get_executor()};
 }
-// end::listen[]
 
-// tag::run_server[]
-cobalt::promise<void> run_server(cobalt::wait_group & workers)
-{
-  auto l = listen(); // <1>
-  while (true)
-  {
-    if (workers.size() == 10u)
-      co_await workers.wait_one();  // <2>
-    else
-      workers.push_back(echo(co_await l)); // <3>
-  }
+cobalt::promise<void> run_server(cobalt::wait_group & workers) {
+    auto l = listen();
+    while (true)
+    {
+        if (workers.size() == 10u)
+            co_await workers.wait_one();
+        else
+            workers.push_back(echo(co_await l));
+    }
 }
-// end::run_server[]
 
-// tag::main[]
-cobalt::main co_main(int argc, char ** argv)
-{
-  co_await cobalt::with(cobalt::wait_group(), &run_server); // <1>
-  co_return 0u;
 }
-// end::main[]
+
+cobalt::main co_main(int argc, char ** argv) {
+    co_await cobalt::with(cobalt::wait_group(), &run_server);
+    co_return 0u;
+}

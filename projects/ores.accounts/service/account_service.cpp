11bd9223626d296aa772fbaeab25bfcc93b76030
Marco Craveiro <marco.craveiro@gmail.com>
2025-10-21 12:44:27 +0100

    [accounts] First stab at implementing the account service

---
+++ projects/ores.accounts/service/account_service.cpp
@@ -0,0 +1,122 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * Copyright (C) 2025 Marco Craveiro <marco.craveiro@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+
+#include "account_service.hpp"
+
+#include <stdexcept>
+#include <boost/uuid/uuid_io.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include "ores.accounts/security/password_manager.hpp"
+#include "ores.utility/log/logger.hpp"
+
+namespace {
+
+using namespace ores::utility::log;
+auto lg(logger_factory("ores.accounts.service.account_service"));
+
+}
+
+namespace ores::accounts::service {
+
+account_service::account_service(repository::account_repository account_repo,
+    repository::logins_repository logins_repo)
+    : account_repo_(std::move(account_repo)),
+      logins_repo_(std::move(logins_repo)) {
+}
+
+domain::account account_service::
+create_account(context ctx, const std::string& username, const std::string& email,
+    const std::string& password, bool is_admin) {
+    // Validate input parameters
+    if (username.empty()) {
+        BOOST_LOG_SEV(lg, error) << "Username cannot be empty";
+        throw std::invalid_argument("Username cannot be empty");
+    }
+    if (email.empty()) {
+        BOOST_LOG_SEV(lg, error) << "Email cannot be empty";
+        throw std::invalid_argument("Email cannot be empty");
+    }
+    if (password.empty()) {
+        BOOST_LOG_SEV(lg, error) << "Password cannot be empty";
+        throw std::invalid_argument("Password cannot be empty");
+    }
+
+    // Generate a new UUID for the account
+    boost::uuids::random_generator gen;
+    auto id = gen();
+
+    // Hash the password using the password manager
+    auto password_hash = security::password_manager::create_password_hash(password);
+
+    // Create the account object with computed fields
+    domain::account new_account {
+        .version = 0,                    // version - will be set by repository
+        .modified_by = "system",         // modified_by - initially system
+        .id = id,
+        .username = username,
+        .password_hash = password_hash,
+        .password_salt = "", // FIXME remove
+        .totp_secret = "",
+        .email = email,
+        .is_admin = is_admin
+    };
+
+    // Create and store the account
+    std::vector<domain::account> accounts{new_account};
+    account_repo_.write(ctx, accounts);
+
+    // Create a corresponding login tracking entry
+    domain::logins login_info{
+        .account_id = id,
+        .last_ip = {},
+        .last_attempt_ip = {},
+        .failed_logins = 0,
+        .locked = false,
+        .last_login = {},
+        .online = false
+    };
+
+    std::vector<domain::logins> logins{login_info};
+    logins_repo_.write(ctx, logins);
+
+    return new_account;
+}
+
+std::vector<domain::account> account_service::list_accounts(context ctx) {
+    return account_repo_.read_latest(ctx);
+}
+
+void account_service::delete_account(context ctx, const boost::uuids::uuid& account_id) {
+    // For now we'll just read the account and return - in a real implementation
+    // this would remove the account from the database, but that requires
+    // additional repository method support
+    auto accounts = account_repo_.read_latest(ctx, account_id);
+    if (accounts.empty()) {
+        BOOST_LOG_SEV(lg, warn) << "Attempted to delete non-existent account: "
+                                << boost::uuids::to_string(account_id);
+        throw std::invalid_argument("Account does not exist");
+    }
+
+    // TODO: In a complete implementation, this would remove the account
+    // and its associated login tracking information from the database
+    BOOST_LOG_SEV(lg, warn) << "Account deletion not fully implemented";
+}
+
+}
---
+++ projects/ores.accounts/service/account_service.hpp
@@ -0,0 +1,96 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * Copyright (C) 2025 Marco Craveiro <marco.craveiro@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+
+#ifndef ORES_ACCOUNTS_SERVICE_ACCOUNT_SERVICE_HPP
+#define ORES_ACCOUNTS_SERVICE_ACCOUNT_SERVICE_HPP
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+#pragma once
+#endif
+
+#include <string>
+#include <boost/uuid/uuid.hpp>
+#include "ores.accounts/domain/account.hpp"
+#include "ores.accounts/repository/account_repository.hpp"
+#include "ores.accounts/repository/logins_repository.hpp"
+
+namespace ores::accounts::service {
+
+/**
+ * @brief Service for managing user accounts including creation, listing, and deletion.
+ */
+class account_service {
+public:
+    using context = ores::utility::repository::context;
+
+    /**
+     * @brief Constructs an account_service with required repositories and security components.
+     *
+     * @param account_repo The repository for managing account data.
+     * @param logins_repo The repository for managing login tracking data.
+     */
+    account_service(repository::account_repository account_repo,
+                    repository::logins_repository logins_repo);
+
+    /**
+     * @brief Creates a new account with the provided details.
+     *
+     * This method receives non-computed fields of the account entity such as
+     * email, password, username, etc. It uses the password manager to compute
+     * the salt and hash, uses the account repository to create the account,
+     * and adds a new login tracking entry.
+     *
+     * @param ctx Repository context for database operations
+     * @param username The unique username for the account
+     * @param email The email address for the account
+     * @param password The plaintext password (will be hashed)
+     * @param is_admin Whether the account should have administrative privileges
+     * @return The created account with computed fields
+     */
+    domain::account create_account(context ctx,
+                                  const std::string& username,
+                                  const std::string& email,
+                                  const std::string& password,
+                                  bool is_admin = false);
+
+    /**
+     * @brief Lists all accounts in the system.
+     *
+     * @param ctx Repository context for database operations
+     * @return Vector of all accounts
+     */
+    std::vector<domain::account> list_accounts(context ctx);
+
+    /**
+     * @brief Deletes an account by its ID.
+     *
+     * @param ctx Repository context for database operations
+     * @param account_id The ID of the account to delete
+     */
+    void delete_account(context ctx, const boost::uuids::uuid& account_id);
+
+private:
+    repository::account_repository account_repo_;
+    repository::logins_repository logins_repo_;
+};
+
+}
+
+#endif

e093635dda6df8f056a3b5f8125db7048880875a
Marco Craveiro <marco.craveiro@gmail.com>
2025-10-21 10:32:50 +0100

    [accounts] Add repository entities

---
+++ projects/ores.accounts/repository/logins_entity.cpp
@@ -0,0 +1,38 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * Copyright (C) 2025 Marco Craveiro <marco.craveiro@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+#include <ostream>
+#include <boost/uuid/uuid_io.hpp>
+#include "ores.accounts/repository/logins_entity.hpp"
+
+namespace ores::accounts::repository {
+
+std::ostream& operator<<(std::ostream& s, const logins_entity& v) {
+    s << "logins_entity{account_id=" << v.account_id.value()
+      << ", last_ip=" << v.last_ip.to_string()
+      << ", last_attempt_ip=" << v.last_attempt_ip.to_string()
+      << ", failed_logins=" << v.failed_logins
+      << ", locked=" << v.locked
+      << ", last_login=" << v.last_login.str()
+      << ", online=" << v.online
+      << "}";
+    return(s);
+}
+
+}
---
+++ projects/ores.accounts/repository/logins_entity.hpp
@@ -0,0 +1,54 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * Copyright (C) 2025 Marco Craveiro <marco.craveiro@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+#ifndef ORES_ACCOUNTS_REPOSITORY_LOGINS_ENTITY_HPP
+#define ORES_ACCOUNTS_REPOSITORY_LOGINS_ENTITY_HPP
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+#pragma once
+#endif
+
+#include <boost/uuid/uuid.hpp>
+#include <boost/asio/ip/address.hpp>
+#include "sqlgen/Timestamp.hpp"
+#include "sqlgen/PrimaryKey.hpp"
+
+namespace ores::accounts::repository {
+
+/**
+ * @brief Represents login tracking information in the database.
+ */
+struct logins_entity {
+    constexpr static const char* schema = "oresdb";
+    constexpr static const char* tablename = "logins";
+
+    sqlgen::PrimaryKey<boost::uuids::uuid> account_id;
+    boost::asio::ip::address last_ip;
+    boost::asio::ip::address last_attempt_ip;
+    int failed_logins;
+    bool locked;
+    sqlgen::Timestamp<"%Y-%m-%d %H:%M:%S"> last_login;
+    bool online;
+};
+
+std::ostream& operator<<(std::ostream& s, const logins_entity& v);
+
+}
+
+#endif

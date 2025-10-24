1987504b22424a7b97dbdf1230496460aeb40852
Marco Craveiro <marco.craveiro@gmail.com>
2025-10-24 01:06:45 +0100

    [accounts] Add mapper for feature flags

---
+++ projects/ores.accounts/repository/feature_flags_mapper.cpp
@@ -0,0 +1,84 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * Copyright (C) 2025 Marco Craveiro <marco.craveiro@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+#include <algorithm>
+#include "ores.utility/log/logger.hpp"
+#include "ores.accounts/repository/feature_flags_mapper.hpp"
+
+namespace {
+
+using namespace ores::utility::log;
+auto lg(logger_factory("ores.accounts.repository.feature_flags_mapper"));
+
+}
+
+namespace ores::accounts::repository {
+
+domain::feature_flags feature_flags_mapper::map(const feature_flags_entity& v) {
+    BOOST_LOG_SEV(lg, debug) << "Mapping db entity: " << v;
+
+    domain::feature_flags r;
+    r.name = v.name.value();
+    r.enabled = v.enabled != 0 ? true : false;
+    r.description = v.description;
+    r.modified_by = v.modified_by;
+
+    BOOST_LOG_SEV(lg, debug) << "Mapped db entity. Result: " << r;
+    return r;
+}
+
+feature_flags_entity feature_flags_mapper::map(const domain::feature_flags& v) {
+    BOOST_LOG_SEV(lg, debug) << "Mapping domain entity: " << v;
+
+    feature_flags_entity r;
+    r.name = v.name;
+    r.enabled = v.enabled;
+    r.description = v.description;
+    r.modified_by = v.modified_by;
+
+    BOOST_LOG_SEV(lg, debug) << "Mapped domain entity. Result: " << r;
+    return r;
+}
+
+std::vector<domain::feature_flags>
+feature_flags_mapper::map(const std::vector<feature_flags_entity>& v) {
+    BOOST_LOG_SEV(lg, debug) << "Mapping db entities. Total: " << v.size();
+
+    std::vector<domain::feature_flags> r;
+    r.reserve(v.size());
+    std::ranges::transform(v, std::back_inserter(r),
+        [](const auto& ve) { return map(ve); });
+
+    BOOST_LOG_SEV(lg, debug) << "Mapped db entities.";
+    return r;
+}
+
+std::vector<feature_flags_entity>
+feature_flags_mapper::map(const std::vector<domain::feature_flags>& v) {
+    BOOST_LOG_SEV(lg, debug) << "Mapping domain entities. Count: " << v.size();
+
+    std::vector<feature_flags_entity> r;
+    r.reserve(v.size());
+    std::ranges::transform(v, std::back_inserter(r),
+        [](const auto& ve) { return map(ve); });
+    BOOST_LOG_SEV(lg, debug) << "Mapped domain entities.";
+    return r;
+}
+
+}
---
+++ projects/ores.accounts/repository/feature_flags_mapper.hpp
@@ -0,0 +1,49 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * Copyright (C) 2025 Marco Craveiro <marco.craveiro@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+#ifndef ORES_ACCOUNTS_REPOSITORY_FEATURE_FLAGS_MAPPER_HPP
+#define ORES_ACCOUNTS_REPOSITORY_FEATURE_FLAGS_MAPPER_HPP
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+#pragma once
+#endif
+
+#include <vector>
+#include "ores.accounts/domain/feature_flags.hpp"
+#include "ores.accounts/repository/feature_flags_entity.hpp"
+
+namespace ores::accounts::repository {
+
+/**
+ * @brief Maps feature flags domain model entities to data storage layer and vice-versa.
+ */
+class feature_flags_mapper {
+public:
+    static domain::feature_flags map(const feature_flags_entity& v);
+    static feature_flags_entity map(const domain::feature_flags& v);
+
+    static std::vector<domain::feature_flags>
+    map(const std::vector<feature_flags_entity>& v);
+    static std::vector<feature_flags_entity>
+    map(const std::vector<domain::feature_flags>& v);
+};
+
+}
+
+#endif

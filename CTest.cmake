9e9e1678387b442fdb708dfa0d7b4bbaf1d13c4e
Marco Craveiro <marco.craveiro@gmail.com>
2025-10-26 19:35:19 +0000

    [cmake] Point overlays to source directory in ctest

--- CTest.cmake
+++ CTest.cmake
@@ -274,7 +274,7 @@ if(${operative_system} STREQUAL "linux")
     message(STATUS "Using dynamic libraries and distro Qt.")
     set(cmake_args ${cmake_args} "-DVCPKG_TARGET_TRIPLET=x64-linux-dynamic")
     set(cmake_args ${cmake_args} "-DBUILD_SHARED_LIBS=ON")
-    set(cmake_args ${cmake_args} "-DVCPKG_OVERLAY_PORTS=build/cmake/overlays/")
+    set(cmake_args ${cmake_args} "-DVCPKG_OVERLAY_PORTS=${CTEST_SOURCE_DIRECTORY}/build/cmake/overlays/")
 endif()
 
 message(STATUS "CMake args: ${cmake_args}")

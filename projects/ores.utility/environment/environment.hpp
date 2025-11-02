abd36f4448aa8002ce08a9f270f4ca14158233d4
Marco Craveiro <marco.craveiro@gmail.com>
2025-11-02 22:32:36 +0000

    [utility] First stab at implementing env vars support

---
+++ projects/ores.utility/environment/environment.hpp
@@ -0,0 +1,67 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * Copyright (C) 2025 Marco Craveiro <marco.craveiro@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+#ifndef ORES_UTILITY_ENVIRONMENT_ENVIRONMENT_HPP
+#define ORES_UTILITY_ENVIRONMENT_ENVIRONMENT_HPP
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+#pragma once
+#endif
+
+#include <string>
+#include <optional>
+
+namespace ores::utility::environment {
+
+/**
+ * @brief Utilities for reading environment variables.
+ */
+class environment final {
+public:
+    /**
+     * @brief Gets an environment variable value.
+     *
+     * @param name Name of the environment variable.
+     * @return The value if found, empty optional otherwise.
+     */
+    static std::optional<std::string> get(const std::string& name);
+
+    /**
+     * @brief Gets an environment variable value with a default.
+     *
+     * @param name Name of the environment variable.
+     * @param default_value Default value if not found.
+     * @return The value if found, default_value otherwise.
+     */
+    static std::string get_or(const std::string& name,
+        const std::string& default_value);
+
+    /**
+     * @brief Gets an environment variable value as an integer.
+     *
+     * @param name Name of the environment variable.
+     * @param default_value Default value if not found or not parseable.
+     * @return The value if found and parseable, default_value otherwise.
+     */
+    static int get_int_or(const std::string& name, int default_value);
+};
+
+}
+
+#endif

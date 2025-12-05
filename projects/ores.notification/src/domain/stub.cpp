4845abe6f43a5f7594b1411d100d34bf17fc968b
Marco Craveiro <marco.craveiro@gmail.com>
2025-12-05 14:09:23 +0000

    [notifcation] Create new component

---
+++ projects/ores.notification/src/domain/stub.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include "ores.notification/domain/stub.hpp"
+
+namespace ores::notification::domain {
+
+std::string stub_function() {
+    return "STUB code to be removed";
+}
+
+}
---
+++ projects/ores.notification/tests/test_stub.cpp
@@ -0,0 +1,39 @@
+/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+ *
+ * Copyright (C) 2025 Marco Craveiro <marco.craveiro@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+#include "ores.notification/domain/stub.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+#include <faker-cxx/faker.h> // IWYU pragma: keep.
+#include "ores.utility/log/make_logger.hpp"
+
+namespace {
+
+const std::string test_suite("ores.notification.tests");
+const std::string tags("[domain]");
+
+}
+
+using namespace ores::notification::domain;
+using namespace ores::utility::log;
+
+TEST_CASE("create_stub", tags) {
+    auto lg(make_logger(test_suite));
+    BOOST_LOG_SEV(lg, warn) << stub_function();
+}

82c861590938d00e47451bbd7728b8c3aacc8fbf
Marco Craveiro <marco.craveiro@gmail.com>
2025-12-12 08:39:44 +0000

    [sql] Improve template creation

--- projects/sql/setup_template.sql
+++ projects/sql/setup_template.sql
@@ -17,9 +17,9 @@
  * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
  */
-
 -- Drop template database if it exists
-drop database if exists oresdb_template;
+-- update pg_database set datistemplate = false where datname = 'oresdb_template';
+-- drop database if exists oresdb_template;
 
 -- Create template database with clean template0 as base
 create database oresdb_template with template = template0;
@@ -31,6 +31,7 @@ grant all privileges on database oresdb_template to ores;
 \c oresdb_template
 
 -- Run all schema creation scripts
+\ir ./create_database.sql
 \ir ./create_all.sql
 
 -- Mark database as template to prevent accidental direct connections

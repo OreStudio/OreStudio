e89324d1166d60dbf0dad7b4f8414c6b496a9df6
Marco Craveiro <marco.craveiro@gmail.com>
2026-01-07 23:21:05 +0000

    [sql] Add script to recreate database in one go

---
+++ projects/ores.sql/recreate_database.sql
@@ -0,0 +1,33 @@
+/* -*- sql-product: postgres; tab-width: 4; indent-tabs-mode: nil -*-
+ *
+ * Copyright (C) 2025 Marco Craveiro <marco.craveiro@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+
+/**
+ * Runs a clean up for the current database and then gets it into a ready state.
+ */
+\pset pager off
+\timing
+
+\ir clean_slate.sql
+\ir setup_user.sql
+\ir admin/setup_admin.sql
+\ir setup_template.sql
+\ir create_instance.sql
+\ir populate/populate.sql
+\ir populate/reference_data.sql
